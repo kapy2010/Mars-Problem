78776c43aa51443e602a83e2a1413c7dd6738785
kapy2010 <kpsingh201091@gmail.com>
2015-09-04 12:01:51 +0530

    Added unit tests

--- mars.rb
+++ mars.rb
@@ -1,5 +1,7 @@
 class MarsRover
   def initialize(coordinates, max_coordinates)
+    max_coordinates = max_coordinates.split(" ")
+    coordinates = coordinates.split(" ")
     @max_x = max_coordinates[0].to_i
     @max_y = max_coordinates[1].to_i
     @x = coordinates[0].to_i
@@ -44,36 +46,40 @@ class MarsRover
       end
     end
 
-    puts "Rovers falled off!" if fall_off == 1
+    if fall_off == 1
+      return "fall"
+    else
+      return nil
+    end
 
   end
 
-  def display
+  def navigate(directions)
+    directions = directions.split("")
+    directions.each { |direction| return "Rovers falled off!" if self.move(direction) == "fall" }
     return "#{@x} #{@y} #{@head}"
   end
 
 end
 
 
-puts "Enter the number of rovers"
-number_of_rovers = gets.chomp.to_i
-
-while number_of_rovers > 0
-
-  puts "Enter the dimensions of plateau"
-  user_input = gets.chomp
-  max_coordinates = user_input.split(" ")
-
-  puts "Enter the position of rover"
-  user_input = gets.chomp
-  coordinates = user_input.split(" ")
-
-  puts "Enter the letters for navigation"
-  direction_input = gets.chomp
-  directions = direction_input.split("")
-  mars = MarsRover.new(coordinates, max_coordinates)
-  directions.each { |direction| mars.move(direction) }
-  puts "Rovers position: #{mars.display}"
-
-  number_of_rovers -= 1
-end
+# puts "Enter the number of rovers"
+# number_of_rovers = gets.chomp.to_i
+#
+# while number_of_rovers > 0
+#
+#   puts "Enter the dimensions of plateau"
+#   max_coordinates = gets.chomp
+#
+#   puts "Enter the position of rover"
+#   coordinates = gets.chomp
+#
+#   puts "Enter the letters for navigation"
+#   direction_input = gets.chomp
+#
+#   mars = MarsRover.new(coordinates, max_coordinates)
+#
+#   puts "Rovers position: #{mars.navigate(direction_input)}"
+#
+#   number_of_rovers -= 1
+# end
